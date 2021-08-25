33a90e0ee7fe601675ef89e53f8e3a7b36ce830a
Akbar Hossain Robin <akbarhossain15@gmail.com>
2021-08-25 12:11:38 +0600

    refactor tip-calculator function

--- public/script.js
+++ public/script.js
@@ -1,41 +1,46 @@
 document.getElementById('tip-button').addEventListener('click', function (event) {
-    if (event.target.value == 'Custom') {
+    var $input = event.target.value;
+    if ($input === 'Custom') {
         document.getElementById('custom-tip-div').style.display = "block";
     }
     else {
-        if (event.target.value != NaN || event.target.value == null) {
-            document.getElementById('total-tip').innerText = "Invalid Input";
-            document.getElementById('per-person').innerText = "Invalid Input";
-        }
-        else {
-            const bill = parseFloat(document.getElementById('bill').value);
-            const people = parseInt(document.getElementById('people').value);
-            const tipPersentage = parseFloat(event.target.value);
-            const tip = (bill * tipPersentage)/100;
-            let totalTip = (bill+ tip);
-            let totalTipPerPerson = tip / people;
-            let totalBillPerPerson = totalTip / people;
-            document.getElementById('total-tip').innerText = totalBillPerPerson.toFixed(2);
-            document.getElementById('per-person').innerText = totalTipPerPerson.toFixed(2); 
-        }   
-    }
-})
-// Catch Custom Tip Button and Reset Button 
+        let bill = parseFloat(document.getElementById('bill').value);
+        let people = parseInt(document.getElementById('people').value);
 
+        let [billPerPerson, tipPerPerson]
+            = calculateTip(bill, people, parseFloat($input), true);
+
+        document.getElementById('total-tip').innerText = billPerPerson.toFixed(2);
+        document.getElementById('per-person').innerText = tipPerPerson.toFixed(2);
+    }
+});
+
+// Catch Custom Tip Button and Reset Button
 const customButtonTip = document.getElementById('custom-tip-btn');
 const resetButton = document.getElementById('reset')
 
 customButtonTip.addEventListener('click', function () {
-        const bill = parseFloat(document.getElementById('bill').value);
-        const people = parseInt(document.getElementById('people').value);
-        const tip = parseFloat(document.getElementById('custom-tip').value);
-        const totalTip = (bill+ tip);
-        const totalTipPerPerson = tip / people;
-        const totalBillPerPerson = totalTip / people;
-        document.getElementById('total-tip').innerText = totalBillPerPerson.toFixed(2);
-        document.getElementById('per-person').innerText = totalTipPerPerson.toFixed(2);
+    let bill = parseFloat(document.getElementById('bill').value);
+    let people = parseInt(document.getElementById('people').value);
+    let tipAmount = parseFloat(document.getElementById('custom-tip').value);
+
+    let [billPerPerson, tipPerPerson] = calculateTip(bill, people, tipAmount);
+
+    document.getElementById('total-tip').innerText = billPerPerson.toFixed(2);
+    document.getElementById('per-person').innerText = tipPerPerson.toFixed(2);
 })
+
 resetButton.addEventListener('click', function () {
     document.getElementById('total-tip').innerText = "0.00";
     document.getElementById('per-person').innerText = "0.00";
 })
+
+function calculateTip(bill = 0, people = 0, tip = 0, percentage = false) {
+    if (percentage) {
+        tip = (bill * tip) / 100;
+    }
+    let billPerPerson = (bill + tip) / people;
+    let tipPerPerson = tip / people;
+
+    return [billPerPerson, tipPerPerson];
+}
